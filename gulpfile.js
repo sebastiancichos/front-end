33e283212d2eaab0ff4bd917b2a079a71e245fd3
Sebastian Cichos <sebastiancichos@outlook.com>
2016-08-30 22:33:27 +0200

    add nunjucks system template

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
-var gulp      = require('gulp'),
-    ts        = require('gulp-typescript'),
-    sass      = require('gulp-sass');
+var gulp     = require('gulp'),
+    ts       = require('gulp-typescript'),
+    sass     = require('gulp-sass'),
+    nunjucks = require('gulp-nunjucks-render');
 
 var paths = {
+    nunjucks: {
+        src: ['src/nunjucks/pages/**/*.+(html|nunjucks)'],
+        built: 'built',
+        watch: 'src/nunjucks/**/*.+(html|nunjucks)',
+        partials: 'src/nunjucks/partials'
+    },
     typescripts: {
         src: ['src/typescript/**/*.ts', '!src/typescript/**/_*.ts'],
-        built: './built/js'
+        built: 'built/assets/js',
+        watch: 'src/typescript/**/*.ts'
     },
     sass: {
         src: ['src/scss/**/*.scss', '!src/scss/**/_*.scss'],
-        built: './built/css'
+        built: 'built/assets/css',
+        watch: ''
     },    
     images: {
         src: ['src/images/**/*', '!src/images/**/*.svg']
@@ -18,15 +27,24 @@ var paths = {
 
 gulp.task('default', ['compile', 'watch']);
 
-gulp.task('compile', ['sass', 'typescript']);
+gulp.task('compile', ['nunjucks', 'sass', 'typescript']);
 
 gulp.task('watch', function() {
-    gulp.watch(paths.sass.src,  { usePolling: true }, ['sass']);
-    gulp.watch(paths.typescripts.src, ['typescript']);
+    gulp.watch(paths.nunjucks.watch, ['nunjucks']);
+    gulp.watch(paths.sass.watch, ['sass']);
+    gulp.watch(paths.typescripts.watch, ['typescript']);
+});
+
+gulp.task('nunjucks', function() {
+    return gulp.src(paths.nunjucks.src)
+        .pipe(nunjucks({
+            path: paths.nunjucks.partials
+        }))
+        .pipe(gulp.dest(paths.nunjucks.built))
 });
 
 gulp.task('sass', function () {
-    gulp.src(paths.sass.src)
+    return gulp.src(paths.sass.src)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(paths.sass.built));
 });
@@ -35,7 +53,7 @@ gulp.task('typescript', function () {
     return gulp.src(paths.typescripts.src)
         .pipe(ts({
             noImplicitAny: true,
-            out: 'output.js'
+            out: 'main.js'
         }))
         .pipe(gulp.dest(paths.typescripts.built));
 });
